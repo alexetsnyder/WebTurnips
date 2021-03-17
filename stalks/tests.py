a2379d7bfb409e403b949f53e5068350a48849b3
Alex E.T. Snyder <alexetsnyder@gmail.com>
2021-03-16 23:11:24 -0400

    Ran and updated tests so that everything runs, and all the tests pass.

--- stalks/tests.py
+++ stalks/tests.py
@@ -13,7 +13,7 @@ class StalkWeekModel(TestCase):
         stalk_week = factory.create_stalk_week(timezone.now(), 90)
         factory.create_turnip_stack(stalk_week_id=stalk_week.id, sell_date=timezone.now(), sell_price=0)
         turnip_stacks = stalk_week.merge_turnips()
-        self.assertEqual(turnip_stacks, [(10, 0)])
+        self.assertEqual(turnip_stacks, [(10, 0, 0)])
 
     def test_merge_turnips_many_stacks(self):
         stalk_week = factory.create_stalk_week(timezone.now(), 90)
@@ -21,7 +21,7 @@ class StalkWeekModel(TestCase):
         factory.create_turnip_stack(stalk_week_id=stalk_week.id, sell_date=timezone.now(), sell_price=100)
         factory.create_turnip_stack(stalk_week_id=stalk_week.id, sell_date=timezone.now(), sell_price=100)
         factory.create_turnip_stack(stalk_week_id=stalk_week.id, sell_date=timezone.now(), sell_price=100)
-        self.assertEqual(stalk_week.merge_turnips(), [(40, 4000)])
+        self.assertEqual(stalk_week.merge_turnips(), [(40, 100, 4000)])
 
     def test_merge_turnips_many_different_stacks(self):
         stalk_week = factory.create_stalk_week(timezone.now(), 90)
@@ -32,6 +32,6 @@ class StalkWeekModel(TestCase):
         factory.create_turnip_stack(stalk_week_id=stalk_week.id, sell_date=timezone.now(), sell_price=0)
         factory.create_turnip_stack(stalk_week_id=stalk_week.id, sell_date=timezone.now(), sell_price=0)
         turnip_stacks = stalk_week.merge_turnips()
-        self.assertIn((20, 2000), turnip_stacks)
-        self.assertIn((20, 2400), turnip_stacks)
-        self.assertIn((20, 0), turnip_stacks)
+        self.assertIn((20, 100, 2000), turnip_stacks)
+        self.assertIn((20, 120, 2400), turnip_stacks)
+        self.assertIn((20, 0, 0), turnip_stacks)
